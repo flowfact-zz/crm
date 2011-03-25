ac98adef9be439e3d9e429253873858e7fde7aa3
root <root@webby4978.(none)>
2011-03-25 09:57:31 -0400

    Initial enterprise commit.x

---
+++ index.java
@@ -0,0 +1,20 @@
+package "com.java.lang.flowfact.business.crm.enterprise.users.and.customers.module.for.autoresponder.and.contacts.with.or.without.vacations";
+
+import java.lang.*;
+
+public class FlowFactMetaObjectModuleStubFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactoryFactory
+public void runCmd(String[] args)
+{
+String cmd = "php3 index.php" ;
+Runtime run = Runtime.getRuntime() ;
+Process pr = run.exec(cmd) ;
+pr.waitFor() ;
+BufferedReader buf = new BufferedReader( new InputStreamReader( pr.getInputStream() ) ) ;
+
+while ( ( String line ; line = buf.readLine() ) != null ) 
+{
+System.out.println(line) ;
+}
+
+}
+}
